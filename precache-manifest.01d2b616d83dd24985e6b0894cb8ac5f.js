3191e242695742f175d790aee33c47ce7d581b86
Adrian Mostowski <adaxelx@o2.pl>
2019-04-20 23:10:59 +0200

    Updates

---
+++ precache-manifest.01d2b616d83dd24985e6b0894cb8ac5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b87063f0c0092ef314dc",
+    "url": "/Strona-salonu/static/js/runtime~main.bd05b77a.js"
+  },
+  {
+    "revision": "8e21fb12a9b52da6672c",
+    "url": "/Strona-salonu/static/js/main.a1628322.chunk.js"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/Strona-salonu/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "8e21fb12a9b52da6672c",
+    "url": "/Strona-salonu/static/css/main.ca4a08ff.chunk.css"
+  },
+  {
+    "revision": "6c1531145f4bdbb6e123daa540dc50e2",
+    "url": "/Strona-salonu/index.html"
+  }
+];
