e0f3169a2739dcbeb8e728f068922b02531b8eaa
Adrian Mostowski <adaxelx@o2.pl>
2019-04-20 23:23:59 +0200

    add logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
 import NavM from './MobileComponents/Nav/NavM.js'
-
+import Logo from './MobileComponents/Logo.js'
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Logo/>
         <NavM/>
       </div>
     );
---
+++ src/MobileComponents/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/MainStyle.sass'
+
+import img from '../images/logo-salon.png'
+
+const Logo = () => {
+    return (
+        <header className="logo">Glynt</header>
+     );
+}
+
+export default Logo;
