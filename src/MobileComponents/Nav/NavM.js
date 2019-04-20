21b76854a2d28866c32fd1dbaab3a506c6f605c7
Adrian Mostowski <adaxelx@o2.pl>
2019-04-20 16:49:04 +0200

    install sass + add navbtn sass file

--- src/MobileComponents/Nav/NavM.js
+++ src/MobileComponents/Nav/NavM.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import NavBtn from './btnNav.js'
 
 class NavM extends React.Component {
-    state = {  }
+    state = {
+        active: false,
+    }
+
+    handleClick = () =>{
+        this.setState({
+            active: !this.state.active
+        })
+    }
+
     render() {
         return (
-
+            <NavBtn active={this.state.active} handleClick={this.handleClick}/>
          );
     }
 }
---
+++ src/MobileComponents/Nav/btnNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../../Styles/NavBtn.module.sass';
+
+const btNav = (props) => {
+    return (
+        <button className={styles.btn}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+     );
+}
+
+export default btNav;
