fd7689ff32381a19c3adddfc925d91016d532050
Adrian Mostowski <adaxelx@o2.pl>
2019-04-20 17:38:18 +0200

    add content to nav open with styles

--- src/MobileComponents/Nav/openedNav.js
+++ src/MobileComponents/Nav/openedNav.js
@@ -6,9 +6,9 @@ const openedNav = (props) => {
     return (
         <nav className={props.active ? 'active' : ''}>
             <ul>
-                <li>Start</li>
-                <li>Oferta</li>
-                <li>Umów się!</li>
+                <li>Start <span>00</span></li>
+                <li>Oferta <span>01</span></li>
+                <li>Umów się! <span>02</span></li>
             </ul>
         </nav>
      );
