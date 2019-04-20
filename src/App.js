eccc6034895878d38068e2a28faffc05a9d0e203
Adrian Mostowski <adaxelx@o2.pl>
2019-04-20 16:35:21 +0200

    add nav files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import NavM from './MobileComponents/Nav/NavM.js'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavM/>
       </div>
     );
   }
---
+++ src/MobileComponents/Nav/NavM.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NavM extends React.Component {
+    state = {  }
+    render() {
+        return (
+
+         );
+    }
+}
+
+export default NavM;
