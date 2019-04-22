04941b5db351c0ab18847f019a1d6ac24bb52e85
Adrian Mostowski <adaxelx@o2.pl>
2019-04-22 22:06:44 +0200

    add home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import NavM from './MobileComponents/Nav/NavM.js'
 import Logo from './MobileComponents/Logo.js'
+import Home from './MobileComponents/HomeM.js'
+import {Switch,BrowserRouter as Router,Route} from 'react-router-dom'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+     <>
         <Logo/>
-        <NavM/>
-      </div>
+          <NavM/>
+        <Router>
+          <Switch>
+            <Route link='/' exact component={Home}/>>
+          </Switch>
+        </Router>
+     </>
     );
   }
 }
---
+++ src/MobileComponents/HomeM.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Styles/MainStyle.sass'
+import img from '../images/salon2.jpg'
+const Home = () => {
+    return (
+        <>
+            <section className='home'>
+                <img src={img} alt=""/>
+            </section>
+        </>
+     );
+}
+
+export default Home;
