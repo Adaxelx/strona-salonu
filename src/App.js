9f3158baa5ec85fe788b2bb3a8097a8fdce573fc
Adrian Mostowski <adaxelx@o2.pl>
2019-04-25 21:09:15 +0200

    add react-transition-group + remove logo.svg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import NavM from './MobileComponents/Nav/NavM.js'
 import Logo from './MobileComponents/Logo.js'
 import Home from './MobileComponents/HomeM.js'
-import {Switch,BrowserRouter as Router,Route} from 'react-router-dom'
+import {Switch,BrowserRouter as Router,Route,Redirect} from 'react-router-dom'
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//      <>
+//         <Router>
+//           <Route render={({ location }) => (
+//             <Route exact path="/" render={() => (
+//               <Redirect to="/start"/>
+//             )}/>
+
+//               <Logo/>
+//               <NavM/>
+//             <TransitionGroup>
+//             <CSSTransition
+//                   key={location.key}
+//                   classNames="fade"
+//                   timeout={1000}>
+//               <Switch location={location}>
+//                 <Route link='/' exact component={Home}/>>
+//               </Switch>
+//            </CSSTransition>
+//            </TransitionGroup>
+//             )}/>
+//         </Router>
+//      </>
+//     );
+//   }
+// }
+
+
+class App extends React.Component{
   render() {
     return (
-     <>
-        <Logo/>
+       <Router>
+        <Route render={({ location }) => (
+          <div>
+            <Route exact path="/" render={() => (
+              <Redirect to="/start"/>
+            )}/>
+          <Logo/>
           <NavM/>
-        <Router>
-          <Switch>
-            <Route link='/' exact component={Home}/>>
-          </Switch>
-        </Router>
-     </>
-    );
-  }
+                <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  classNames="fade"
+                  timeout={1000}
+                >
+            <Switch location={location}>
+                <Route exact path="/start" exact component={Home} />
+                <Route exact path="/oferta" exact component={Home} />
+            </Switch>
+            </CSSTransition>
+        </TransitionGroup>
+
+
+          </div>
+        )}/>
+      </Router>
+     );
+}
 }
 
 export default App;
+
