c9491a0e58cbacdaba82bc67f91cc93077987140
Adrian Mostowski <adaxelx@o2.pl>
2019-04-25 22:50:03 +0200

    add animation on scroll to title in offer(+small fix nav)

--- src/MobileComponents/Offer.js
+++ src/MobileComponents/Offer.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import '../Styles/MainStyle.sass'
-const Offer = () => {
-    return (
+
+class Offer extends React.Component{
+
+    handleScroll = () => {
+        const scrollY = window.scrollY;
+        console.log(this.title.current.offsetTop)
+        if(scrollY>this.title.current.offsetTop + this.title.current.offsetHeight*2 -window.innerHeight){
+            this.title.current.classList.add('active')
+        }
+    }
+
+    title = React.createRef();
+
+    componentDidMount(){
+        window.addEventListener('scroll',this.handleScroll)
+    }
+
+    render(){
+        return (
         <main className='offer'>
+            <h1 ref={this.title} className='title'>Nasza oferta</h1>
         </main>
-     );
+        );
+    }
+
 }
 
 export default Offer;
