5e94cf83717b519ddd5e0e3c1e5376677b1f9d06
Adrian Mostowski <adaxelx@o2.pl>
2019-04-27 15:50:39 +0200

    Add content to partnership

--- src/MobileComponents/Partnership.js
+++ src/MobileComponents/Partnership.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import '../Styles/MainStyle.sass'
+
+import logo from '../images/glynt.png'
+
 class Partnership extends React.Component {
     state = {  }
+
+    handleScroll = ()=>{
+        const {item} = this
+        const scrollV = window.scrollY
+
+
+
+
+            if(scrollV > item.current.offsetTop - window.innerHeight + item.current.offsetHeight/2){
+                item.current.classList.add('active')
+            }else{
+                item.current.classList.remove('active')
+            }
+    }
+
+    item = React.createRef();
+
+    componentDidMount(){
+        window.addEventListener('scroll',this.handleScroll)
+    }
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.handleScroll)
+    }
+
     render() {
         return (
             <section className='partnership'>
                 <h2>Pracujemy na najlepszej jakości kosmetykach marki <span>Glynt</span></h2>
+                <img ref={this.item} src={logo} alt=""/>
+                <p>Kosmetyki firmy <span>Glynt</span> są w 100% oparte na wegańskich składnikach!</p>
             </section>
          );
     }
