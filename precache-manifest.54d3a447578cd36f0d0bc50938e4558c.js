1be2410bed1d875449af02fe58c6cfb59c71a0c1
Adrian Mostowski <adaxelx@o2.pl>
2019-04-30 22:12:27 +0200

    Updates

--- precache-manifest.54d3a447578cd36f0d0bc50938e4558c.js
+++ precache-manifest.54d3a447578cd36f0d0bc50938e4558c.js
@@ -4,27 +4,27 @@ self.__precacheManifest = [
     "url": "/strona-salonu/static/media/salon.6db37e50.png"
   },
   {
-    "revision": "f7cf2132a10ba71438ef7c816b74da9d",
-    "url": "/strona-salonu/static/media/glynt.f7cf2132.png"
+    "revision": "740efdc2fe8a1abdcdc112a68060bc74",
+    "url": "/strona-salonu/static/media/glynt.740efdc2.png"
   },
   {
     "revision": "9c7c04716640c5eab7f1",
     "url": "/strona-salonu/static/js/runtime~main.a5c54488.js"
   },
   {
-    "revision": "978ac7a46cd8609eabab",
-    "url": "/strona-salonu/static/js/main.5b5bf277.chunk.js"
+    "revision": "923639a16b91d5d78368",
+    "url": "/strona-salonu/static/js/main.651fe07a.chunk.js"
   },
   {
     "revision": "2c1cf91713901b6ad956",
     "url": "/strona-salonu/static/js/2.7e142b11.chunk.js"
   },
   {
-    "revision": "978ac7a46cd8609eabab",
-    "url": "/strona-salonu/static/css/main.8fe3219c.chunk.css"
+    "revision": "923639a16b91d5d78368",
+    "url": "/strona-salonu/static/css/main.b5ed449b.chunk.css"
   },
   {
-    "revision": "6467aff20eee63aa395fb4493c645ab6",
+    "revision": "000d2f995e6959016ed78da0a24698cc",
     "url": "/strona-salonu/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/strona-salonu/precache-manifest.6e69ddd795caeabb6fabd5a6579c90b6.js"
+  "/strona-salonu/precache-manifest.54d3a447578cd36f0d0bc50938e4558c.js"
 );
 
 workbox.clientsClaim();
