d514328ae1602f8ebf2416d5793a18516350e3c1
Adrian Mostowski <adaxelx@o2.pl>
2019-05-01 19:57:37 +0200

    some visual styles to nav desktop

--- src/DesktopComponents/navBtnDesk.js
+++ src/DesktopComponents/navBtnDesk.js
@@ -4,7 +4,14 @@ class Nav extends React.Component {
     state = {  }
     render() {
         return (
-            <nav className='deskNav'></nav>
+            <nav className='deskNav'>
+            <ul>
+                <li>Start</li>
+                <li>O mnie</li>
+                <li>Umów się!</li>
+            </ul>
+            <p>tel. 503 432 435</p>
+            </nav>
          );
     }
 }
