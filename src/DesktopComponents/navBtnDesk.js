99addd0a848e768e4a9cc873b59d406179db469c
Adrian Mostowski <adaxelx@o2.pl>
2019-05-02 20:48:41 +0200

    nav desktop hover animations

--- src/DesktopComponents/navBtnDesk.js
+++ src/DesktopComponents/navBtnDesk.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../Styles/MainStyle.sass'
+import {NavLink} from 'react-router-dom'
+
 class Nav extends React.Component {
     state = {  }
     render() {
         return (
             <nav className='deskNav'>
             <ul>
-                <li>Start</li>
-                <li>O mnie</li>
-                <li>Umów się!</li>
+               <NavLink exact to='/'><li>Start</li></NavLink>
+               <NavLink exact to='/offer'><li>O mnie</li></NavLink>
+                <NavLink exact to='/contact'><li>Umów się!</li></NavLink>
             </ul>
             <p>tel. 503 432 435</p>
             </nav>
