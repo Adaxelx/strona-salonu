e43552b63b9a66dd077d5e2360b52732843eee36
Adrian Mostowski <adaxelx@o2.pl>
2019-05-23 23:04:27 +0200

    some styles in about(titles and text)

--- src/MobileComponents/AboutPage.js
+++ src/MobileComponents/AboutPage.js
@@ -8,7 +8,8 @@ class AboutMe extends React.Component {
         return (
             <section className='aboutPage'>
                 <h1 className='title'><span className='firstLetter'>O</span> mnie</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste hic aperiam voluptates debitis libero mollitia voluptate vitae, ea voluptatem aut recusandae incidunt quibusdam, cumque ab ratione saepe autem. Ratione, vitae.</p>
+                <p className='intro'>Nazywam się Beata Patejuk i fryzjerstwo od zawsze było moją pasją. Prowadzę własny salon od września 2017 roku, wcześniej od XX lat pracowałam w innych salonach gdzie też zdobywałam doświadczenie.</p>
+                <h2 className='subtitle'><span className='firstLetter'>U</span>kończone kursy: </h2>
             </section>
          );
     }
