ddc0c48e3f653e9d9c5c1839bc87cbb18d1737c3
Adrian Mostowski <adaxelx@o2.pl>
2019-05-25 13:41:05 +0200

    some styles + form in contactpage

--- src/MobileComponents/ContactPage.js
+++ src/MobileComponents/ContactPage.js
@@ -3,15 +3,27 @@ import React from 'react';
 import Footer from './Footer.js'
 
 class ContactPage extends React.Component {
-    state = {  }
+    state = {
+        username: '',
+        number: '',
+        message:'',
+     }
 
-        scrollTop = () => {
-            window.scrollTo(0, 0)
-        }
+     handleChange = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value,
+        })
+     }
 
-        componentDidMount() {
-            setTimeout(this.scrollTop, 500);
-        }
+    scrollTop = () => {
+        window.scrollTo(0, 0)
+    }
+
+    componentDidMount() {
+        setTimeout(this.scrollTop, 500);
+    }
 
     render() {
         return (
@@ -20,10 +32,27 @@ class ContactPage extends React.Component {
                 <section className='contactSection'>
                 <h2 className="title"><span className='firstLetter'>U</span>mów się!</h2>
                 <section className='contactTypes'>
-                    <h3>Możesz to zrobić poprzez:</h3>
-                    <p><i className="fas fa-mobile-alt"></i> 666 000 333</p>
-                    <p>Ważne! Proszę dzwonić tylko w godzinach pracy.</p>
+                    <h3 className='subtitle'>Możesz to zrobić poprzez:</h3>
+                    <p className='telephoneNumer'><i className="fas fa-mobile-alt"></i> 666 000 333</p>
+                    <aside><span>Ważne!</span> Proszę dzwonić tylko w godzinach pracy (podane w sekcji poniżej).</aside>
                 </section>
+                <form action="">
+                    <h3 className='subtitle'>Lub za pomocą formularza:</h3>
+                    <label className='name' htmlFor="user">
+                        <p>Imię i Nazwisko</p>
+                        <input placeholder='Imię i nazwisko' type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange}></input>
+                    </label>
+                    <label className='number' htmlFor="phone-number">
+                        <p>Numer telefonu</p>
+                        <input placeholder='Numer telefonu' type="number" id="number" name="number" value={this.state.email} onChange={this.handleChange}></input>
+                    </label>
+                    <label className='message' htmlFor="message">
+                        <p>Rodzaj usługi i przybliżona data</p>
+                        <textarea id="message" name="message" value={this.state.message} onChange={this.handleChange}></textarea>
+                    </label>
+                    <button>Send</button>
+                    <p>Data wizyty zostanie podana podczas rozmowy telefonicznej, ewetnualnie wysłana przez SMS lub mail w zależności od preferencji klienta.</p>
+                </form>
                 </section>
                 <Footer/>
             </article>
