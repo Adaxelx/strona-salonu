a5662f550c714b67dbef810c82c2974b980d9a72
Adrian Mostowski <adaxelx@o2.pl>
2019-05-25 20:06:20 +0200

    add on scroll event to gallery

--- src/MobileComponents/Images.js
+++ src/MobileComponents/Images.js
@@ -17,15 +17,44 @@ import img14 from '../img gallery/14.jpg'
 import img15 from '../img gallery/15.jpg'
 
 let imgs = [img,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14, img15]
+let imgRefs = []
 
-const Images = (props) => {
-    const images =  imgs.map(img => <img onClick={props.showImg} src={img} key={img} alt=""/>);
+class Images extends React.Component{
 
+
+    showImg = () => {
+
+        const scrollV = window.scrollY
+
+        imgRefs.forEach(ref => {
+            if(scrollV > ref.current.offsetTop - window.innerHeight + ref.current.offsetHeight/2){
+                ref.current.classList.add('active')
+            }
+            else{
+                ref.current.classList.remove('active')
+            }
+        })
+    }
+
+    componentDidMount(){
+        for(let i=0;i<15;i++){
+            imgRefs[i] = React.createRef();
+        }
+        window.addEventListener('scroll',this.showImg)
+        setTimeout(this.showImg,200)
+    }
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.showImg)
+    }
+
+    render(){
+    const images =  imgs.map((img,i) => <img ref={imgRefs[i]} src={img} key={img} alt=""/>);
     return (
         <section className='images'>
             {images}
         </section>
      );
+    }
 }
 
 export default Images;
